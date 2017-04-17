18a1519e8236a67323cd6fc1d8e64b4504ce177e
Dorian.Li <dorian.li@sap.com>
2017-04-17 15:20:34 +0800

    sap-ui-preload

--- Add_launch_pad_url_params.user.js
+++ Add_launch_pad_url_params.user.js
@@ -5,8 +5,8 @@
 // @description  add cb language switcher params
 // @author       Dorian Li
 // @downloadURL  https://github.com/leedorian/userscripts/raw/master/Add_launch_pad_url_params.user.js
-// @match        http://qp2wdp.nanshan.com.tw:8888/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html*
-// @match        http://dp2wdp.nanshan.com.tw:8888/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html*
+// @match        http://qp6wdp.nanshan.com.tw:8888/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html*
+// @match        http://dp6wdp.nanshan.com.tw:8888/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html*
 // @match        https://agsdev02.mo.sap.corp:8443/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html*
 // @grant        none
 // ==/UserScript==
@@ -15,7 +15,7 @@
     'use strict';
 
     var oldParam = window.location.search;
-    var switches = ["sap-ui-xx-componentPreload=off","sap-ushell-nocb=true","sap-language=ZF"];
+    var switches = ["sap-ui-preload=debug","sap-ui-xx-componentPreload=off","sap-ushell-nocb=true","sap-language=ZF"];
     var newSwitches = [];
     var newParam = "";
     if(oldParam !== ""){
